a0552b838d58f71294c6a5d86953371edf1d46c6
Michael Mesguich <michael.mesguich@radcom.com>
2021-01-02 21:22:01 +0200

    [master] initial commit {2021-01-02 19:22}

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,8 @@
+//import ReactDOM from 'react-dom';
+//import App from './App';
+
+//I commented below
+
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
@@ -6,3 +11,23 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+//it('renders without crashing', () => {
+//    const div = document.createElement('div');
+//    ReactDOM.render(<App />, div);
+//    ReactDOM.unmountComponentAtNode(div);
+//});
+//
+//it('renders without crashing', () => {
+//    const div = document.createElement('div');
+//    ReactDOM.render(<App />, div);
+//    ReactDOM.unmountComponentAtNode(div);
+//});
+
